b2e955269606c0d2f96d7030829e8c81923cad39
Mark Beinker <mwb@quantlink.de>
2020-06-26 19:28:58 +0200

    Version number and documentation updated

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,20 @@
 //!     println!("Apple's quotes in January: {:?}", quotes);
 //! }
 //! ```
+//! Another method to retrieve a range of quotes is by
+//! requesting the quotes for a given period and lookup frequency. Here is an example retrieving the daily quotes for the last month:
+//! ```rust
+//! use yahoo_finance_api as yahoo;
+//! use std::time::{Duration, UNIX_EPOCH};
+//! use chrono::{Utc,TimeZone};
+//! 
+//! fn main() {
+//!     let provider = yahoo::YahooConnector::new();
+//!     let response = provider.get_quote_range("AAPL", "1d", "1mo").unwrap();
+//!     let quotes = response.quotes().unwrap();
+//!     println!("Apple's quotes of the last month: {:?}", quotes);
+//! }
+//! ```
 
 use chrono::{DateTime, Utc};
 use reqwest::StatusCode;
