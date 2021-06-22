d5124612e211d3ff49d4922979e5203d571f0e80
Xavier Gillard <xavier.gillard@uclouvain.be>
2021-06-23 01:20:34 +0200

    support for market events (splits and dividends)

--- src/lib.rs
+++ src/lib.rs
@@ -181,12 +181,12 @@ const YSEARCH_URL: &str = "https://query2.finance.yahoo.com/v1/finance/search";
 // Macros instead of constants,
 macro_rules! YCHART_PERIOD_QUERY {
     () => {
-        "{url}/{symbol}?symbol={symbol}&period1={start}&period2={end}&interval={interval}"
+        "{url}/{symbol}?symbol={symbol}&period1={start}&period2={end}&interval={interval}&events=div|split"
     };
 }
 macro_rules! YCHART_RANGE_QUERY {
     () => {
-        "{url}/{symbol}?symbol={symbol}&interval={interval}&range={range}"
+        "{url}/{symbol}?symbol={symbol}&interval={interval}&range={range}&events=div|split"
     };
 }
 macro_rules! YTICKER_QUERY {
